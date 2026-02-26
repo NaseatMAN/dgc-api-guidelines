8c57adc84700cfb3943c86a162e29dd180ef29ad
NaseatBlend <naseatman.developer@outlook.com>
2026-02-26 14:31:27 +0700

    rename wrong folder name

--- src/DGC.Sample.Application/Interfaces/Repositoies/IBaseRepository.cs
+++
@@ -1,9 +0,0 @@
-namespace DGC.Sample.Application.Interfaces.Repositoies
-{
-    public interface IBaseRepository<TEntity>
-        where TEntity : class
-    {
-        IQueryable<TEntity> Query();
-        IQueryable<TEntity> QueryAsNoTracking();
-    }
-}
--- src/DGC.Sample.Application/Interfaces/Repositoies/IRepository.cs
+++
@@ -1,7 +0,0 @@
-namespace DGC.Sample.Application.Interfaces.Repositoies
-{
-    public interface IRepository<TEntity> : IReaderRepository<TEntity>, IWriterRepository<TEntity>
-        where TEntity : class
-    {
-    }
-}
