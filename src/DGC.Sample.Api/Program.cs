b45f732d7ce278a1bc872fde1bea9db200dd32b2
IsaacJReay <isaacjacksonreay@gmail.com>
2026-04-08 16:23:15 +0700

    chore: comment code

--- src/DGC.Sample.Api/Program.cs
+++ src/DGC.Sample.Api/Program.cs
@@ -1,8 +1,8 @@
 using DGC.Sample.Api.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
-// Optional authentication samples are registered once; endpoint attributes decide at compile time whether an action requires OIDC, API key, both, or neither.
-builder.Services.ConfigureApiAuthentication(builder.Configuration);
+
+//builder.Services.ConfigureApiAuthentication(builder.Configuration);
 builder.Services.AddApiControllersWithAzureValidation();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -18,9 +18,8 @@ var app = builder.Build();
 app.UseApiMiddlewares();
 app.UseSwaggerConfiguration();
 app.UseHttpsRedirection();
-// Authentication must run before authorization when sample auth attributes are applied to endpoints.
-app.UseAuthentication();
-app.UseAuthorization();
+//app.UseAuthentication();
+//app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
