b6fb56fdab7ebc4041ff497035eb3a3aaf431567
IsaacJReay <isaacjacksonreay@gmail.com>
2026-04-08 14:04:08 +0700

    chore: remove unused code

--- src/DGC.Sample.Application/Features/Orders/Commands/CreateOrderCommand.cs
+++
@@ -1,9 +0,0 @@
-using DGC.Sample.Domain.Enums;
-
-namespace DGC.Sample.Application.Features.Orders.Commands;
-
-public sealed record CreateOrderCommand(
-    string CustomerName,
-    DateTime OrderDateUtc,
-    OrderStatus Status,
-    decimal TotalAmount);
--- src/DGC.Sample.Application/Features/Orders/Queries/GetOrderByIdQuery.cs
+++
@@ -1,3 +0,0 @@
-namespace DGC.Sample.Application.Features.Orders.Queries;
-
-public sealed record GetOrderByIdQuery(Guid Id);
